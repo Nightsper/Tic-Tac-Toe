d3b02edac7d1a6ef79bc9262ec016302a34f64af
Nightsper <fahimhossain645@gmail.com>
2025-10-09 05:06:49 +0600

    Create board and player objects

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ return {board}
 function player (name, marker) {
   return {name, marker}
 }
+
+function gameFlow() {
+  let gameboard = createBoard()
+  let player1 = player("player1", "X");
+  let player2 = player("player2", "O");
+}
