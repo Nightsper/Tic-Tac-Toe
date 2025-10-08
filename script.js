e00416104e3757a779390fd08d0f67672d0a1287
Nightsper <fahimhossain645@gmail.com>
2025-10-09 05:17:32 +0600

    Logic for first player turn

--- script.js
+++ script.js
@@ -14,11 +14,22 @@ function gameFlow() {
   let player1 = player("player1", "X");
   let player2 = player("player2", "O");
   
+  let whoFirst = prompt("Who goes first? Enter 1 or 2")
+  let currentPlayer;
+  
+  if (whoFirst == "1") {
+    currentPlayer = player1
+  }
+    else if (whoFirst == "2") {
+      currentPlayer = player2
+    }
+      else (currentPlayer = player1)
+  
   function placeMarker(i) {
     if (gameboard.board.array[i] == "X" || gameboard.board.array[i] == "O") {
       return
     }
-      else (gameboard.board.array[i] = player1.marker)
+      else (gameboard.board.array[i] = currentPlayer.marker)
 }
 
 return {placeMarker}
