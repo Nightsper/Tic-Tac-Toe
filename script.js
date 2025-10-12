b1925266d2d4526a76bfc8cc51ce86938c6d572e
Nightsper <fahimhossain645@gmail.com>
2025-10-12 08:54:06 +0600

    Added win logic and board's UI

--- script.js
+++ script.js
@@ -1,45 +1,97 @@
 function createBoard() {
-let board = {
-  array: ["", "", "", "", "", "", "", "", ""]
-}
-return {board}
+  let board = {
+    array: ["", "", "", "", "", "", "", "", ""]
+  }
+  return { board }
 }
 
-function player (name, marker) {
-  return {name, marker}
+function player(name, marker, marks) {
+  return { name, marker, marks }
 }
 
 function gameFlow() {
   let gameboard = createBoard()
-  let player1 = player("player1", "X");
-  let player2 = player("player2", "O");
+  let player1 = player("player1", "X", []);
+  let player2 = player("player2", "O", []);
+  let currentPlayer = player1;
   
-  let whoFirst = prompt("Who goes first? Enter 1 or 2")
-  let currentPlayer;
-  
-  if (whoFirst == "1") {
-    currentPlayer = player1
-  }
-    else if (whoFirst == "2") {
-      currentPlayer = player2
-    }
-      else (currentPlayer = player1)
+  let boardUI = document.querySelector("#board")
+  let tiles = document.querySelectorAll(".tiles")
   
   function placeMarker(i) {
     if (gameboard.board.array[i] == "X" || gameboard.board.array[i] == "O") {
       return
     }
-      else (gameboard.board.array[i] = currentPlayer.marker)
+    else if (gameboard.board.array[i] == "") {
+      gameboard.board.array[i] = currentPlayer.marker
       
-    if (currentPlayer == player1) {
-        currentPlayer = player2
+      currentPlayer.marks.push(i)
+      
+      boardUI.children[i].textContent = currentPlayer.marker
     }
-      else if (currentPlayer == player2) {
+    else {
+      return
+    }
+    
+    checkWin(currentPlayer)
+    
+    switch (currentPlayer) {
+      case player1:
+        currentPlayer = player2
+        break;
+      case player2:
         currentPlayer = player1
-      }
+        break;
+    }
+    
+  }
+  
+  tiles.forEach(tile => {
+    tile.addEventListener("click", function() {
+      placeMarker(this.id)
+    })
+  })
+  
+  return { placeMarker }
 }
 
-return {placeMarker}
+
+function checkWin(currentPlayer) {
+  const winningCases = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  
+  let checkArr = [];
+  
+  winningCases.forEach((winCase) => {
+    
+    winCase.forEach((num) => {
+      
+      for (const marks of currentPlayer.marks) {
+        if (num == marks) {
+          checkArr.push(marks)
+        }
+      }
+    })
+    
+    if (JSON.stringify(winCase) !== JSON.stringify(checkArr)) {
+      
+      checkArr = []
+    }
+    else if (JSON.stringify(winCase) == JSON.stringify(checkArr)) {
+      console.log(`${currentPlayer.name} is the winner!`)
+      
+    }
+    
+  })
+  
 }
 
 const game = gameFlow()
